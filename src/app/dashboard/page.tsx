85df43475c3e0bb0b45b1397aa187039cc773f56
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-04-08 13:46:17 +0530

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import NextImage from "next/image";
+import { ImagesSliderDemo as CustomHome } from "@/components/home";
+
+export default function HomePage() {
+  return (
+    <main>
+      <CustomHome />
+    </main>
+  );
+}
---
+++ src/components/home.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "@/components/ui/images-slider";
+
+export function ImagesSliderDemo() {
+    const images = [
+        "https://images.unsplash.com/photo-1600959907703-125ba1374a12?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1554734867-bf3c00a49371?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1612574935301-af13ccce9258?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ];
+    return (
+        <ImagesSlider className="h-[40rem]" images={images}>
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    y: -80,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    duration: 0.6,
+                }}
+                className="z-50 flex flex-col justify-center items-center"
+            >
+                <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+                    One Stop Solution for all of your <br /> Ambulance Needs
+                </motion.p>
+                <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+                    <span>Book now →</span>
+                    <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+                </button>
+            </motion.div>
+        </ImagesSlider>
+    );
+}
